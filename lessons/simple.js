6aa6ce0dc68b9d7ebd258b1efc93f009a7c847a2
Hector Coronado <hector.coronado@autodesk.com>
2020-06-02 14:42:31 -0700

    abstract test assertions into js assertion lib

--- lessons/simple.js
+++ lessons/simple.js
@@ -2,14 +2,24 @@ const {sum, subtract} = require('../math')
 
 let result, expected
 
+const expect = actual => {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`)
+      } else {
+        console.log(`${actual} is equal to ${expected}`)
+      }
+    }
+  }
+}
+
 result = sum(3, 7)
 expected = 10
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
-}
+
+expect(result).toBe(expected);
 
 result = subtract(7, 3)
 expected = 4
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
-}
+
+expect(result).toBe(expected);
